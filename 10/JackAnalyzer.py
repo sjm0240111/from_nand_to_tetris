d42d6348242cc2d1efc0d821210c6f2ac32a4c6a
John Lee <718048411@qq.com>
2017-08-04 12:13:55 +0800

    word processing

--- 10/JackAnalyzer.py
+++ 10/JackAnalyzer.py
@@ -39,10 +39,16 @@ def openfile(file):
 def analyzefile(jackfile):
     with open(jackfile, 'r') as myfile:
         text = myfile.read()
-    
+    comment = r'//.*\n|/\*.*?\*/'
+    symbolptn = r'(\{)|(\})|(\()|(\))|(\[)|(\])|(\.)|(\,)|(;)|(\+)|'\
+    +'(\-)|(\*)|(/)|(&)|(\|)|(<)|(>)|(=)|(~)'
+    symbolrpl = r' \1\2\3\4\5\6\7\8\9\10\11\12\13\14\15\16\17\18\19 '
+    text = re.sub(comment, ' ',text)
+    text = re.sub(symbolptn, symbolrpl, text)
+    wordlist = re.split(r'\s+', text)
     namevm = re.sub(r'.jack','.vm',jackfile)
     fout = open(namevm, 'w')
     
     
-def analyzeline(line):
+
     
